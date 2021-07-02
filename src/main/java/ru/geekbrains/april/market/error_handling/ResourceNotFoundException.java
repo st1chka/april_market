164cf291f6cf7f70550aacc4d21ceacd3ee5fb63
st1chka <kalinine03@gmail.com>
2021-07-02 11:01:01 +0300

    delet all

--- src/main/java/ru/geekbrains/april/market/error_handling/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.april.market.error_handling;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
